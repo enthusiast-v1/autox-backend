a02306ae3483d40ac5e2cc76aa217dcc80bf500d
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-26 19:50:50 +0600

    User registration implementation d one with proper validation

---
+++ src/app/modules/auth/interface.ts
@@ -0,0 +1,25 @@
+import { Gender, User } from '@prisma/client';
+
+type TUserWithoutPassword = Omit<User, 'password'>;
+
+type TLoginResponse = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+type TRegisterResponse = {
+  user: TUserWithoutPassword;
+} & TLoginResponse;
+
+type TRegister = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  gender: Gender;
+  address?: string;
+  image?: string;
+  contactNo: string;
+};
+
+export { TUserWithoutPassword, TLoginResponse, TRegisterResponse, TRegister };
