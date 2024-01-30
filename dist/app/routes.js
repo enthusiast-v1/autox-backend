9ba71d204b9a8da59d853f752ef2cb60b18731da
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-30 11:49:32 +0600

    Js files commited

--- dist/app/routes.js
+++ dist/app/routes.js
@@ -1,8 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.routes = void 0;
-const express_1 = require("express");
-const route_1 = require("./modules/auth/route");
+const express_1 = require('express');
+const route_1 = require('./modules/auth/route');
 const router = (0, express_1.Router)();
-[{ path: '/auth', route: route_1.AuthRoutes }].forEach(({ path, route }) => router.use(path, route));
+[{ path: '/auth', route: route_1.AuthRoutes }].forEach(({ path, route }) =>
+  router.use(path, route),
+);
 exports.routes = router;
