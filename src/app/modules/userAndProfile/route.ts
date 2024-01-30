1afe195d9566966998208d7d829f5cfaee28f602
ahmad muas <amaahmadmusa@gmail.com>
2024-01-31 01:17:24 +0600

    Added ZOD validation

--- src/app/modules/userAndProfile/route.ts
+++ src/app/modules/userAndProfile/route.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { ProfileController } from './controller';
+import { AuthValidation } from './validation';
 
 const router = express.Router();
 
-router.post('/profile-update', ProfileController.updateProfile);
+const { ZProfileUpdate } = AuthValidation;
+
+router.post(
+  '/profile-update',
+  validateRequest(ZProfileUpdate),
+  ProfileController.updateProfile,
+);
 router.get('/', ProfileController.getUsers);
 router.get('/:id', ProfileController.getSingleUser);
 
---
+++ src/app/modules/userAndProfile/validation.ts
@@ -0,0 +1,22 @@
+import { EGender } from '@prisma/client';
+
+import { z } from 'zod';
+
+const ZProfileUpdate = z.object({
+  body: z.object({
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+    gender: z
+      .enum([...Object.keys(EGender)] as [string, ...string[]], {
+        required_error: 'Gender is required!',
+      })
+      .optional(),
+    address: z.string().optional(),
+    image: z.string().optional(),
+    contactNo: z.string().optional(),
+    emergContact: z.string().optional(),
+    dateOfBirth: z.date().optional(),
+  }),
+});
+
+export const AuthValidation = { ZProfileUpdate };
