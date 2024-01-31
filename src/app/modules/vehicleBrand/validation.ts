41ee14ce48ef89464e12107a06bc1e2f38c99eaa
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-31 19:33:47 +0600

    Deleted rentType and vehicle brand module and updated prisma schema

--- src/app/modules/vehicleBrand/validation.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-const ZCreateVehicleBrand = z.object({
-  body: z.object({
-    brand: z.string({ required_error: 'Brand name is required!' }),
-  }),
-});
-
-export const VehicleBrandValidation = { ZCreateVehicleBrand };
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import { AuthRoutes } from './modules/auth/route';
-import { RentTypeRoutes } from './modules/rentType/route';
 import { BookingRoutes } from './modules/booking/route';
-import { VehicleBrandRoutes } from './modules/vehicleBrand/route';
 import { VehicleRoutes } from './modules/vehicle/route';
 import { DriverRoutes } from './modules/driver/route';
 
@@ -10,9 +8,7 @@ const router = Router();
 
 [
   { path: '/auth', route: AuthRoutes },
-  { path: '/rent-types', route: RentTypeRoutes },
   { path: '/bookings', route: BookingRoutes },
-  { path: '/vehicle-brands', route: VehicleBrandRoutes },
   { path: '/vehicles', route: VehicleRoutes },
   { path: '/drivers', route: DriverRoutes },
 ].forEach(({ path, route }) => router.use(path, route));
