0f0540d5936ecc749cf2655a67833e0ba2b50977
ahmad muas <amaahmadmusa@gmail.com>
2024-02-01 22:44:49 +0600

    Commit message describing your changes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,16 +1,12 @@
 import { Router } from 'express';
 import { AuthRoutes } from './modules/auth/route';
-// import { BookingRoutes } from './modules/booking/route';
 import { ProfileRoutes } from './modules/profile/route';
-// import { RentTypeRoutes } from './modules/rentType/route';
 import { UserRoutes } from './modules/user/route';
 
 const router = Router();
 
 [
   { path: '/auth', route: AuthRoutes },
-  // { path: '/rent-types', route: RentTypeRoutes },
-  // { path: '/bookings', route: BookingRoutes },
   { path: '/profile', route: ProfileRoutes },
   { path: '/user', route: UserRoutes },
 ].forEach(({ path, route }) => router.use(path, route));
